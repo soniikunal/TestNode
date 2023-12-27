1c8d5d9ca58056c72830b8fc844c8e8bb55a5f44
kunal soni <kunal94611@gmail.com>
2023-12-27 20:56:03 +0530

    added cors

--- index.js
+++ index.js
@@ -1,29 +1,48 @@
-import express, { json } from 'express';
-import dotenv from 'dotenv'
+import express, { json } from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import bodyParser from "body-parser";
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(json())
+app.use(json());
+app.use(
+  cors({
+    origin: ["http://localhost:5173"],
+    credentials: true,
+  })
+);
 dotenv.config();
 
-import './DB/connection.js';
+import "./DB/connection.js";
 
-import { authRoutes } from './Routes/auth.js';
-import { teamRoutes } from './Routes/Admin/Team.js';
-import { articleRoutes } from './Routes/Admin/Question/ArticleRoutes.js';
-import { questionRoutes } from './Routes/Admin/Question/QuestionRoutes.js';
+import { authRoutes } from "./Routes/auth.js";
+import { teamRoutes } from "./Routes/Admin/Team.js";
+import { articleRoutes } from "./Routes/Admin/Question/ArticleRoutes.js";
+import { questionRoutes } from "./Routes/Admin/Question/QuestionRoutes.js";
+import { ATDQuestionRoutes } from "./Routes/Admin/Question/ATDQuestionRoutes.js";
+import { categoryRoutes } from "./Routes/Admin/Category.js";
 
-app.use('/auth', authRoutes)
-app.use('/admin', teamRoutes)
-app.use('/admin', articleRoutes)
-app.use('/admin', questionRoutes)
+app.use("/auth", authRoutes);
+app.use("/admin", teamRoutes);
+app.use("/admin", articleRoutes);
+app.use("/admin", questionRoutes);
+app.use("/admin", categoryRoutes);
+app.use("/admin/ATD", ATDQuestionRoutes);
+
+// //Serve static files from the 'uploads' directory
+// app.use("/uploads", express.static("uploads"));
+
+// // Use body-parser middleware with increased payload limit
+// app.use(bodyParser.json({ limit: '10mb' }));
+// app.use(bodyParser.urlencoded({ limit: '10mb', extended: true }));
 
 
 
-app.get('/', (req, res) => {
-    res.send('Welcome to Express Server');
+app.get("/", (req, res) => {
+  res.send("Welcome to Express Server");
 });
 
 app.listen(port, () => {
-    console.log(`Server is Fire at http://localhost:${port}`);
+  console.log(`Server is Fire at http://localhost:${port}`);
 });
